92cc011941651946d9e3df9ece11b96c9b71946a
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-07-25 19:24:56 -0700

    add landing page

---
+++ app/header.tsx
@@ -0,0 +1,30 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+export default function Header() {
+  return (
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <GlobeAltIcon className="h-8 w-8 text-indigo-500" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <div className="py-6">
+            <a
+              href="#"
+              className="-mx-3 rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white"
+            >
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="-mx-3 rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
